247596e77941530dfb0c6f74e586696f5493cd93
Zach Bookey <zabookey@csbsju.edu>
2015-07-07 13:55:43 -0600

    Made it simpler to change execution_space

--- src/KokkosSetup.hpp
+++ src/KokkosSetup.hpp
@@ -13,7 +13,13 @@ This file is for the intention of creating things needed by Kokkos.
 #include "Geometry.hpp" // Just so we have the local_int_t and global_int_t definitions.
 
 //TODO Find a way to change this at compile time.
+typedef Kokkos::Serial execution_space;
+#ifdef HPCG_Kokkos_OpenMP
 typedef Kokkos::OpenMP execution_space;
+#endif
+#ifdef HPCG_Kokkos_Cuda
+typedef Kokkos::Cuda execution_space;
+#endif
 //View typedefs.
 //Normal types.
 typedef Kokkos::View<int *> int_1d_type;
